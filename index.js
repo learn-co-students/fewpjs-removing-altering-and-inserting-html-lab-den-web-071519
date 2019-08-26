fa35e94fbd94afd8cf944c290f29b3298189d8b8
SwanHub <jdprince555@gmail.com>
2019-08-26 15:16:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.getElementById('main')
+document.body.removeChild(main)
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.textContent = "YOUR NAME is the champion."
+newHeader.id = "victory"
