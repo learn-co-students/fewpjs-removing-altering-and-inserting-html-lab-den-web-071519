cc7bf37c5eb21692b8890f4cee71a12ecf7bff6b
Ethan Collins <ethantcollins98@gmail.com>
2019-08-26 14:43:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+
+var element = document.createElement('h1')
+
+document.body.appendChild(element).id = 'victory'
+
+var newHeader = document.querySelector('h1#victory')
+
+newHeader.innerHTML = 'Ethan is the champion'
+
+
+
